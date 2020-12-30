10e53c3efc068065e6e15f392328f141e9616def
Eric Bailey <eric@ericb.me>
2020-12-29 22:08:44 -0600

    Initial commit

---
+++ nix/overlays/emacs.nix
@@ -0,0 +1,16 @@
+_: pkgs: {
+  myEmacs = pkgs.emacsWithPackages (epkgs: (with epkgs.melpaPackages; [
+    better-defaults
+    crux
+    editorconfig
+    fill-column-indicator
+    hl-todo
+    idris-mode
+    multiple-cursors
+    nix-mode
+    smex
+    use-package
+    whitespace-cleanup-mode
+    yaml-mode
+  ]));
+}
